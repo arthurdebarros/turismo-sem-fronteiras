1e76e78f2013a01df965846ce93387196e4704a2
IsraeLucena <israel.lucena@ccc.ufcg.edu.br>
2014-09-10 20:51:52 -0300

    modificacao no codigo para o sistema funcionar como deve ser,

--- tsf/app/controllers/Application.java
+++ tsf/app/controllers/Application.java
@@ -1,43 +1,16 @@
 package controllers;
 
-import models.Participante;
-import models.Viagem;
-import play.*;
-import play.mvc.*;
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+		if (session().get("user") == null) {
+			return redirect(routes.Login.show());
+		}
+        return ok(index.render("Home Page"));
     }
-    
-	public Result criarParticipacao(Viagem v, Participante p){
-		try{
-			v.getEstado().adicionarParticipante(v, p);
-			return ok(index.render("Participante adicionado"));
-		}catch (Exception e){
-			return badRequest();
-		}
-	}
-
-	public Result setSenha(Viagem v, String senha){
-		try{
-			v.getEstado().tornarFechado(v, senha);
-			return ok(index.render("Agora sua viagem é protegida por senha"));
-		}catch (Exception e){
-			return badRequest();
-		}
-	}
-
-	public Result tornarAberto(Viagem v){
-		try{
-			v.getEstado().tornarAberto(v);
-			return ok(index.render("Agora qualquer um pode entrar em sua viagem"));
-		}catch (Exception e){
-			return badRequest();
-		}
-	}
-
 
 }
